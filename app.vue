2017cb98088bd42d72dfe82f84e9859b563b6ddd
rafi021 <mahmud.ibrahim021@gmail.com>
2022-09-22 16:20:40 +0600

    dynamic component -1

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-  <HomeWelcome />
-  <Counter />
+  <button @click="toggle">Toggle</button>
+  <component :is="MyComponent" />
   </div>
 </template>
+
+<script setup>
+  const MyComponent = shallowRef(resolveComponent('Counter'));
+
+  function toggle(){
+    MyComponent.value = resolveComponent('HomeWelcome');
+  }
+</script>

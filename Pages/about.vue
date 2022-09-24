30463c810b22294177f2b3b0a26e470a926d3cf2
rafi021 <mahmud.ibrahim021@gmail.com>
2022-09-24 08:36:39 +0600

    assets directory and install tailwind css

--- Pages/about.vue
+++ Pages/about.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <h1>Hello About!!</h1>
+        <h1 class="text-3xl font-bold">Hello About!!</h1>
     </div>
 </template>
--- Pages/index.vue
+++ Pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, animi voluptate. Corrupti veniam possimus sed ea delectus iure enim consectetur? Ipsam, totam! Debitis maiores, ea deserunt eveniet non vel exercitationem.</h1>
+    <div class="bg-purple-500">
+        <h1 class="text-3xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, animi voluptate. Corrupti veniam possimus sed ea delectus iure enim consectetur? Ipsam, totam! Debitis maiores, ea deserunt eveniet non vel exercitationem.</h1>
     </div>
 </template>

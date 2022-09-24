cd1ac745d1cf21245734c87ee0828d799e4f877a
rafi021 <mahmud.ibrahim021@gmail.com>
2022-09-24 07:39:59 +0600

    Page Navigation

--- Pages/about.vue
+++ Pages/about.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="about">About</NuxtLink>
         <h1>Hello About!!</h1>
     </div>
 </template>
--- Pages/index.vue
+++ Pages/index.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="about">About</NuxtLink>
         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, animi voluptate. Corrupti veniam possimus sed ea delectus iure enim consectetur? Ipsam, totam! Debitis maiores, ea deserunt eveniet non vel exercitationem.</h1>
     </div>
 </template>
